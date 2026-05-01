018f5c767bc0b36a95d1093698410cd675eea8b3
llschall <llschall@gmx.com>
2026-05-01 11:43:45 +0200

    [Repo] Rename as control-board

--- src/main/java/org/llschall/control/board/ControlBoardApplication.java
+++ src/main/java/org/llschall/control/board/ControlBoardApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BootControllerApplication {
+public class ControlBoardApplication {
 
     static void main(String[] args) {
-        SpringApplication.run(BootControllerApplication.class, args);
+        SpringApplication.run(ControlBoardApplication.class, args);
     }
 
 }
