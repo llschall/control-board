f9c1fa0b85b2cb138168659a985f7dce74b680db
llschall <llschall@gmx.com>
2026-05-02 12:22:42 +0200

    [Serial] Integrate ardwloop

---
+++ ino/control-board/control-board.ino
@@ -0,0 +1,32 @@
+
+#include <Ardwloop.h>
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+  
+  // Make the led blink on start up
+  for (int i = 0; i < 3; i++) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(200);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(200);
+  }
+
+  // Must be the same as on the Java side
+  ardw_setup(BAUD_9600);
+}
+
+void loop() {
+  ardw_loop();
+
+  int v = ardw_r()->a.v;
+
+  if (v == 1) {
+    digitalWrite(LED_BUILTIN, HIGH);
+  } else {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+  
+  delay(99);
+}
